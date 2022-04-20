487073b1b0faf9089f29eedda61c5006f795af4c
Michael Alao <contactleomax@gmail.com>
2022-04-20 14:26:30 +0100

    --- Auto-Git Commit ---

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,9 @@ import "./App.css";
 
 import InputTodo from "./components/InputTodo";
 import ListTodos from "./components/ListTodos";
-import LoadingTodos from "./components/LoadingTodos";
 
 function App() {
-  console.log(ListTodos.length);
 
-  // let TodoList = ListTodos.length > 0 ? ListTodos : LoadingTodos;
   return (
     <Fragment>
       <div className="container">
